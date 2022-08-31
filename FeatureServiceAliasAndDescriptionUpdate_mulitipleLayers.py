d1e725594359c507b10dc74ff6ef269da13ea78d
Lisa Berry <lberry@esri.com>
2022-08-30 17:03:24 -0700

    Update FeatureServiceAliasAndDescriptionUpdate_mulitipleLayers.py
    Updated: August 2022 - can also update decimals for popup JSON based on input from excel file. Also updates popupElement in JSON if saved in new Map Viewer

--- FeatureServiceAliasAndDescriptionUpdate_mulitipleLayers.py
+++ FeatureServiceAliasAndDescriptionUpdate_mulitipleLayers.py
@@ -1,23 +1,58 @@
 # Name: Alias Updater
+# Created by: Lisa Berry, Esri
+# Created: December 2018
 #
 # This script uses a lookup table to update alias names on a hosted feature service.
 # The script updates the alias names in two places:
 #   - The REST endpoint
-#   - The layer's pop-up JSON
+#   - The layer's pop-up JSON via fieldInfos
+#   - *If the layer was saved in the new Map Viewer in ArcGIS Online, updates the additional popupElement fieldInfos
 # The pop-up configuration will not be altered with this implementation
-# The script also allows you to update the long description and field type for any field
+# The script also allows you to update the long description, field type, and pop-up decimals for any field
 #
-# The script will use the input excel document and update any fields it finds that matches from the excel document
+# The script will use the input excel document and update any fields it finds that matches from the excel document.
+# This script allows for multiple REST layers to be updated. Specify the REST layer count in the inputs.
+# You must have ArcGIS Pro installed on your computer in order to run this script.
 #
-# This script allows for multiple REST layers to be updated. Specify the REST layer count in the inputs
-#
-# You must have ArcGIS Pro installed on your computer in order to run this script
-# Python version: 3.6. Make sure your interpreter is calling to the arcgispro-py3 python.exe
-#
-# Created by: Lisa Berry, Esri
-# Created: December 2018
+# Python version: 3.7 - Make sure your interpreter is calling to the arcgispro-py3 python.exe
 # Updated: April 2020 - all http calls removed and replaced with python API calls
 # Updated: July 2022 - Converted XLRD to OPENPYXL to read in excel file. XLRD no longer supports .xlsx files.
+# Updated: August 2022 - can also update decimals for popup JSON.
+#          Also updates popupElement in JSON if saved in new Map Viewer
+
+# Comments about inputs:_________________________________________________________________________________________
+# username and password are your ArcGIS Online or ArcGIS Enterprise credentials
+#
+# layerID is the ID to a hosted feature service. You must own the service to run this script.
+#
+# restLayerCount is the count of layers in the service. All layers will use
+#               the same field/alias lookup, but only matching fields will be updated.
+#
+# lookupTable must be an excel document (.xlsx) with a header row. 
+#   The first column should be the field names
+#   The second column should be the intended alias names for each field.
+#   *optional* The third column should be the intended description for each field.
+#   *optional* The fourth column can include the field type. This must be formatted
+#           to match the backend JSON. 
+#           Ex:  nameOrTitle, description, typeOrCategory, countOrAmount, percentageOrRatio
+#               measurement, currency, uniqueIdentifier, phoneNumber, emailAddress,
+#               orderedOrRanked, binary, locationOrPlaceName, coordinate, dateAndTime
+#   *optional* The fifth column can include a specification for how many decimals you want for each field
+#               to have in the pop-up.
+# If your script is having issues, make sure you at least have these 5 headers in the excel document,
+# even if no values appear in the rows. This can cause the script to fail sometimes.
+
+# portalName can be left as-is if you are working in ArcGIS Online. Change to your portal URL otherwise.
+
+# Inputs:_______________________________________________________________________________________________________
+username = "username"
+password = "password"
+layerID = "itemID"
+restLayerCount = 1
+lookupTable = r"C:\pathName\ExcelDocName.xlsx"
+portalName = "https://www.arcgis.com"
+
+# MAIN SCRIPT___________________________________________________________________________________________________
 
 from arcgis import gis
 from arcgis.features import FeatureLayer
@@ -25,34 +60,6 @@ import openpyxl
 from copy import deepcopy
 import os
 import copy
-
-# Comments about inputs:
-# username and password are your ArcGIS Online organizational credentials
-
-# layerID is the ID to a hosted feature service 
-
-# restLayerCount is the count of layers in the service. All layers will use
-#               the same field/alias lookup, but only matching fields will be updated.
-
-# lookupTable must be an excel document (.xlsx) with a header row. 
-# The first column should be the field names
-# The second column should be the intended alias names for each field.
-# *optional* The third column should be the intended description for each field.
-# *optional* The fourth column can include the field type. This must be formatted
-#           to match the backend JSON. 
-#           Ex:  nameOrTitle, description, typeOrCategory, countOrAmount, percentageOrRatio
-#               measurement, currency, uniqueIdentifier, phoneNumber, emailAddress,
-#               orderedOrRanked, binary, locationOrPlaceName, coordinate, dateAndTime
-
-# portalName can be left as-is if you are working in ArcGIS Online.
-
-# Inputs:
-username = "username"
-password = "password"
-layerID = "itemID"
-restLayerCount = 1
-lookupTable = r"C:\path\ExcelDoc.xlsx"
-portalName = "https://www.arcgis.com"
 
 # Login to your arcgis account
 login = gis.GIS(portalName, username, password)
@@ -129,7 +136,7 @@ else:
         # Now check if the item has a pop-up configuration saving the alias names as well
         # First, grab the item JSON for the layer and create an item to hold the new edited JSON
         print("\tUpdating the alias names within the pop-up configuration on the item...")
-        item = gis.Item(login,itemid=layerID)
+        item = gis.Item(login, itemid=layerID)
 
         # Grab the existing JSON for the popup, store a copy, and edit the aliases
         itemJSON = item.get_data(try_json=True)
@@ -138,12 +145,44 @@ else:
         if itemJSON:
             print("\tFinding all replacements of alias names within pop-up...")
             newItemJSON = copy.deepcopy(itemJSON)
+            print("\t\tUpdating alias names in popup fieldInfos...")
             for i in itemJSON['layers'][looper]['popupInfo']['fieldInfos']: #change [0] to whatever layer you're working on (1,2,3)
                 fieldName2 = i['fieldName']
                 for lookup in lookupList:
                     if lookup[0] == fieldName2:
                         newItemJSON['layers'][looper]['popupInfo']['fieldInfos'][counter]['label'] = lookup[1] #change [0] to whatever layer you're working on (1,2,3)
+                        # Check if there is a decimal spec
+                        if "format" in i and "places" in i["format"]:
+                            # If a value is specified in the lookup doc, assign that
+                            if lookup[4]:
+                                newItemJSON['layers'][looper]['popupInfo']['fieldInfos'][counter]['format']['places'] = lookup[4]
+                            # If a value is not specified and the decimals have defaulted to 6, change to 2
+                            else:
+                                if newItemJSON['layers'][looper]['popupInfo']['fieldInfos'][counter]['format']['places'] == 6:
+                                    newItemJSON['layers'][looper]['popupInfo']['fieldInfos'][counter]['format']['places'] = 2
                 counter += 1
+
+            # Check if layer was updated in new Map Viewer and contains a popupElement JSON section with fieldInfos
+            if "popupElements" in itemJSON['layers'][looper]['popupInfo'] and itemJSON['layers'][looper]['popupInfo']["popupElements"] and "fieldInfos" in itemJSON['layers'][looper]['popupInfo']["popupElements"][0]:
+                print("\t\tUpdating popupElement fieldInfo...")
+                counter2 = 0
+                for j in itemJSON['layers'][looper]['popupInfo']["popupElements"][0]["fieldInfos"]:
+                    fldName = j["fieldName"]
+                    for lkup in lookupList:
+                        if lkup[0] == fldName:
+                            newItemJSON['layers'][looper]['popupInfo']['popupElements'][0]["fieldInfos"][counter2]['label'] = lkup[1]
+                            # Check if there is a decimal spec
+                            if "format" in j and "places" in j["format"]:
+                                # If a value is specified in the lookup doc, assign that
+                                if lkup[4] != None:
+                                    newItemJSON['layers'][looper]['popupInfo']['popupElements'][0]["fieldInfos"][counter2]['format']['places'] = lkup[4]
+                                # If a value is not specified and the decimals have defaulted to 6, change to 2
+                                else:
+                                    if newItemJSON['layers'][looper]['popupInfo']['popupElements'][0]["fieldInfos"][counter2]['format']['places'] == 6:
+                                        newItemJSON['layers'][looper]['popupInfo']['popupElements'][0]["fieldInfos"][counter2]['format']['places'] = 2
+                    counter2 += 1
+
+
             # Update json
             print("\tUpdating the alias names within the existing item pop-up...")
             portal = portalName
